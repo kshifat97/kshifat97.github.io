ef74efaf05737fa10fb2124a3f04b2ef86e0b043
Kazi Tahsin Mahmood <85328137+kshifat97@users.noreply.github.com>
2021-09-01 01:48:08 +0600

    Website

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 const skillsContent = document.getElementsByClassName('skills__content'),
-      skillsHeader = document.querySelectorAll('skills__header'),
+      skillsHeader = document.querySelectorAll('skills__header')
 
 function toggleSkills(){
     let itemClass = this.parentNode.ClassName
@@ -42,17 +42,34 @@ function toggleSkills(){
         skillsContent[i].className = 'skills_content skills__close'
     }
     if(itemClass === 'skills__content skills__close'){
-        this.parentNode.className = 'skills_-content skills__open'
+        this.parentNode.className = 'skills__content skills__open'
     }
 }
 
-skillsHeader.forEach((el) =>{
+skillsHeader.forEach((eL) =>{
     el.addEventListener('click', toggleSkills)
 })
 
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
 
+tabs.foreach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
