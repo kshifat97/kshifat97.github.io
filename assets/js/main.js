fc0896a58132f36a97f92b6e596493c47fdc909f
Kazi Tahsin Mahmood <85328137+kshifat97@users.noreply.github.com>
2021-09-02 20:43:56 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,10 +108,42 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
     },
   });
 
-function myFunction(city) {
-  var dots = document.querySelector(`.potfolio__data[data-city="${city}"] .dots`);
-  var moreText = document.querySelector(`.potfolio__data[data-city="${city}"] .more`);
-  var btnText = document.querySelector(`.potfolio__data[data-city="${city}"] .myBtn`);
+function myFunction1() {
+  var dots = document.getElementById("dots1");
+  var moreText = document.getElementById("more1");
+  var btnText = document.getElementById("myBtn1");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction2() {
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction3() {
+  var dots = document.getElementById("dots3");
+  var moreText = document.getElementById("more3");
+  var btnText = document.getElementById("myBtn3");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
