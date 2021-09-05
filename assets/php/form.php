3441cdeb86319712f19e227584f9ce50942ece6a
Kazi Tahsin Mahmood <85328137+kshifat97@users.noreply.github.com>
2021-09-05 20:10:13 +0600

    Website

--- assets/php/form.php
+++ assets/php/form.php
@@ -1,25 +1,19 @@
 <?php
-$name - $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
 
-$email_from = 'tahsin148@outlook.com';
-
-$email_subject = "New Form Submission";
-
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                    "User Message: $message.\n";
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['mail'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
 
-$to = "kshifat97@gmail.com";
+    $mailTo = "tahsin148@outlook.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from".$name.".\n\n".$message;
 
-$headers = "from: $email_from \r\n";
 
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: index.html");
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
 
 ?>
