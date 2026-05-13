6e225471883225c323ad15dde6e4044bcd730dac
Kazi Tahsin Mahmood <85328137+kazitmahmood@users.noreply.github.com>
2026-05-12 21:42:46 -0400

    okay

--- .vscode/edge-run-profile/Default/Extensions/dahenjhkoodjbpjheillcadbppiidmhp/0.5.0_0/reloadscript-compiled.js
+++
@@ -1,1 +0,0 @@
-(function(){window.addEventListener("message",b=>{const a=b.data;b.origin==="chrome-extension://dahenjhkoodjbpjheillcadbppiidmhp"&&a&&a instanceof Object&&a.type==="gsrReload"&&window.location.reload()});}).call(this);
